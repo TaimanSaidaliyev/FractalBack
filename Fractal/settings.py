a4fa14c8dbc3535f07fcb806b746adaf3a52fc52
TaimanSaidaliyev <117729843+TaimanSaidaliyev@users.noreply.github.com>
2023-05-27 19:37:09 +0600

    Update settings.py

--- Fractal/settings.py
+++ Fractal/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-!!$0lwqjqqif6)#j(6o1gztpd03jc2=cnoh(d%p2)&967p9c7_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
